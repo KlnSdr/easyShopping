6021481222cf64f1a31685446dd72bdf2ed986c1
KlnSdr <monsieurk1209@gmail.com>
2024-03-24 17:29:09 +0100

    increase version

--- components/InfoScreen/InfoScreen.ts
+++ components/InfoScreen/InfoScreen.ts
@@ -14,7 +14,7 @@ class InfoScreen implements Component {
                 },
                 {
                     tag: 'p',
-                    text: 'v2.0.1',
+                    text: 'v2.0.2',
                 },
             ],
         };
