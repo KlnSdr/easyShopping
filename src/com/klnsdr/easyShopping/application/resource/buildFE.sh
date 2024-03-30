de50d5e6fcd27e414a7f793944a0768dc853620a
KlnSdr <monsieurk1209@gmail.com>
2024-03-30 15:52:10 +0100

    add empty edom project for login page

--- src/com/klnsdr/easyShopping/application/resource/buildFE.sh
+++ src/com/klnsdr/easyShopping/application/resource/buildFE.sh
@@ -2,7 +2,16 @@ cd public || exit
 rm -rf static
 mkdir static
 
+echo "building /"
 cd mainScreen || exit
 ed pack
 cp -r docs/* ../static
+rm -rf docs
+
+echo "building /hades/login"
+cd ../login || exit
+ed pack
+mkdir ../static/hades/
+mkdir ../static/hades/login
+cp -r docs/* ../static/hades/login
 rm -rf docs
