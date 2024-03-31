13c3cd58357f6c1fb8d68aabee7bd4be5c73ef1b
KlnSdr <monsieurk1209@gmail.com>
2024-03-31 15:58:18 +0200

    set version to 2.0.2-EOL

--- components/InfoScreen/InfoScreen.ts
+++ components/InfoScreen/InfoScreen.ts
@@ -14,7 +14,7 @@ class InfoScreen implements Component {
                 },
                 {
                     tag: 'p',
-                    text: 'v2.0.2',
+                    text: 'v2.0.3-EOL',
                 },
             ],
         };
