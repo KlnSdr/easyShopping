909b3a16ae68ddda55e8f922ea6eac89e9154dca
KlnSdr <monsieurk1209@gmail.com>
2024-04-16 23:24:37 +0200

    add hades config options to application.json

--- src/com/klnsdr/easyShopping/application/resource/public/mainScreen/components/InfoScreen/InfoScreen.ts
+++ src/com/klnsdr/easyShopping/application/resource/public/mainScreen/components/InfoScreen/InfoScreen.ts
@@ -14,7 +14,7 @@ class InfoScreen implements Component {
                 },
                 {
                     tag: 'p',
-                    text: 'v3.0.2',
+                    text: 'v3.0.3',
                 },
             ],
         };
