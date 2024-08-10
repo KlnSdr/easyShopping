dcbf8466402f38643eb56ecc217c3ae845951823
KlnSdr <monsieurk1209@gmail.com>
2024-08-10 22:16:18 +0200

    upgrade to hades v0.5 (dobby 1.5, thot 1.7)

--- src/com/klnsdr/easyShopping/application/public/mainScreen/components/InfoScreen/InfoScreen.ts
+++ src/com/klnsdr/easyShopping/application/public/mainScreen/components/InfoScreen/InfoScreen.ts
@@ -14,7 +14,7 @@ class InfoScreen implements Component {
                 },
                 {
                     tag: 'p',
-                    text: 'v3.0.7',
+                    text: 'v3.0.8',
                 },
             ],
         };
