12ffbe01c4bae431c8a5b3b83b1e8a659cafdca6
KlnSdr <monsieurk1209@gmail.com>
2025-01-25 16:59:47 +0100

    add dockerfile and build script

--- src/com/klnsdr/easyShopping/application/public/mainScreen/components/InfoScreen/InfoScreen.ts
+++ src/com/klnsdr/easyShopping/application/public/mainScreen/components/InfoScreen/InfoScreen.ts
@@ -14,7 +14,7 @@ class InfoScreen implements Component {
                 },
                 {
                     tag: 'p',
-                    text: 'v3.0.9',
+                    text: 'v3.9',
                 },
             ],
         };
