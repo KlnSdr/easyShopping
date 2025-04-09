79b44076f7d4144c14d12269d3acece581b46054
KlnSdr <monsieurk1209@gmail.com>
2025-04-09 20:41:30 +0200

    [increase version to 3.11]

--- src/com/klnsdr/easyShopping/application/public/mainScreen/components/InfoScreen/InfoScreen.ts
+++ src/com/klnsdr/easyShopping/application/public/mainScreen/components/InfoScreen/InfoScreen.ts
@@ -14,7 +14,7 @@ class InfoScreen implements Component {
                 },
                 {
                     tag: 'p',
-                    text: 'v3.10',
+                    text: 'v3.11',
                 },
             ],
         };
